561b46ba8d95d907c4267dd4571826b7327f4729
Romer Ventura <roventur@microsoft.com>
2020-05-22 14:55:13 -0500

    url fix

--- express-api/expapi/routes/echo.js
+++ express-api/expapi/routes/echo.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 var apiAdapter = require('./apiAdapter');
 
-const BASE_URL = 'http://localhost:7071'
+const BASE_URL = 'https://rvlabs-echo-api.azurewebsites.net'
 const api = apiAdapter(BASE_URL)
+
+const options = {
+  headers: {'x-functions-key': ''}
+};
 
 /* GET users listing. */
 router.get('/echo', function(req, res, next) {
@@ -11,7 +15,12 @@ router.get('/echo', function(req, res, next) {
 });
 
 router.post('/api/echo_api', (req, res) => {
-  api.post(req.path, req.body).then(resp => {
+  api.post(req.path, req.body, options)
+  .then(resp => {
+    res.send(resp.data)
+  })
+  .catch(resp => {
+    console.log('Error on backend')
     res.send(resp.data)
   })
 })
