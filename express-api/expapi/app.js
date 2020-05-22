a0f3c6ac278b025294167955e8f6c81e62f7f6be
Romer Ventura <roventur@microsoft.com>
2020-05-21 23:23:15 -0500

    node app

--- express-api/expapi/app.js
+++ express-api/expapi/app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(router);
 
-// catch 404 and forward to error handler
+// catcha 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
